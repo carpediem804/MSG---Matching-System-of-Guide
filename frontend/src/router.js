6b5a9b1db9c6258f541eff95f784337e053b1036
hyunho <hyunho0587@gmail.com>
2019-03-28 20:26:38 +0900

    현호

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,23 +1,37 @@
 import Vue from 'vue';
 import Router from 'vue-router'
 import user from './components/user'
+import search from './components/search'
 import Home from './components/HelloWorld'
+import toursearch from "./components/toursearch";
 Vue.use(Router)
 
-export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-      {
-          path: '/user',
-          name: 'user',
-          component: user
-      }
 
-  ]
+export default new Router({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/user',
+            name: 'user',
+            component: user
+        },
+        {
+            path: '/search',
+            name: 'search',
+            component: search
+        },
+        {
+            path: '/toursearch',
+            name: 'toursearch',
+            component: toursearch
+        }
+
+    ]
+
 })
