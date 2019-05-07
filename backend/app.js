a373f6030aa42c97a8db1dca083a081d5c060cb7
carpediem804 <33194900+carpediem804@users.noreply.github.com>
2019-05-07 17:08:47 +0900

    data수정

--- backend/app.js
+++ backend/app.js
@@ -4,18 +4,10 @@ const bodyParser = require('body-parser')
 require('./db/mongo') //connect db
 const getTourList = require('./routes/getTourList')
 const registerTour = require('./routes/registerTour')
-const deletepost = require('./routes/removeblog')
 const recruit = require('./routes/recruit')
 const signup = require('./routes/signup')
 const app = express()
-//var login = require('./routes/login')
 
- // getTourList
- // applyTour
- // registerTour
- // getReqList
- // getReqList
- //
 app.use((req, res, next) =>{
     res.header("Access-Control-Allow-Origin", "*")
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
@@ -30,10 +22,5 @@ app.use('/getTourList', getTourList);
 app.use('/registerTour', registerTour);
 app.use('/registUserInfo',signup);
 app.use('/recruit',recruit);
-//app.use('/delete',deletepost);
-//app.use('/like',like);
-//app.use('/signup',signup);
-//app.use('/login', login);
-
 
 module.exports = app
--- backend/routes/recruit.js
+++ backend/routes/recruit.js
@@ -12,13 +12,15 @@ router.post('/custom', function (req, res, next) {
     //console.log(req);
     console.log("/custom임");
     console.log(req.body.params);
-    let savecostom = new guiderecruit();
-
+    let savecustom = new guiderecruit();
     savecustom.UserID = req.body.params.recruitdata.id;
     savecustom.RecruitTitle = req.body.params.recruitdata.title;
     savecustom.RecruitLocation =req.body.params.recruitdata.location;
     savecustom.RecruitPeopleNumber = req.body.params.recruitdata.number;
     savecustom.RecruitContent = req.body.params.recruitdata.content;
+    savecustom.From_time = Date.now;
+    savecustom.To_time = Date.now;
+    savecustom.ApplyRecruitID = "아직 안정햇다`";
     savecustom.save(function(err,data){
         if(err){
             console.log(err);
