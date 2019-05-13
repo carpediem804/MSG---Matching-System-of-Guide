c84ce5d18abc1b3a10a978987184414254fcf52b
carpediem804 <33194900+carpediem804@users.noreply.github.com>
2019-05-13 20:09:35 +0900

    태홍태홍

--- backend/routes/checkinfo.js
+++ backend/routes/checkinfo.js
@@ -1,14 +1,33 @@
 const { Router } = require('express');
 const router = Router();
 //const Image = require('../db/models/Images');
-const Tourinfo = require('../db/models/Userinfo');
-const applyguide = require('../db/models/Userinfo')
+const Tourinfo = require('../db/models/Tour');
+const applyguide = require('../db/models/GuideRecruit')
 router.post('/register', function(req, res,next){
     console.log("checkinfo/register로 들어옴");
-    console.log(req.body);
+    //console.log(req.body);
     console.log(req.body.params);
-
-    //Tourinfo.find({Email:req.body.user.email}).exec()
+    if(req.body.params.type == "여행객") {
+        console.log("여행객");
+        applyguide.find({UserID: req.body.params.email},function(err,applyguidedata){
+            if(err) {
+                console.log(err);
+            }else{
+            //    console.log(applyguidedata);
+                res.json({applyguidedata});
+            }
+        })
+    }
+    else if(req.body.params.type == "가이드"){
+        console.log("가이드");
+        Tourinfo.find({UserID: req.body.params.email},function(err,Tourdata){
+            if(err) {
+                console.log(err);
+            }else{
+                res.json({Tourdata});
+            }
+        })
+    }
 });
 
 
