e5742ef20323ac06ba942fa5d5498855cde1ce7c
kh06089 <kh06089@ajou.ac.kr>
2019-05-18 14:38:56 +0900

    이쥐쥐 버억 777

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,10 +1,10 @@
 <template>
   <v-ons-page>
     <div class="home" align="center">
-      <img class="back_image" src="../assets/background3.jpg">
-      <img class="back_image" src="../assets/background4.jpg">
       <img class="back_image" src="../assets/background2.jpg">
       <img class="back_image" src="../assets/backgruond1.jpg">
+      <img class="back_image" src="../assets/background4.jpg">
+      <img class="back_image" src="../assets/background3.jpg">
     </div>
   </v-ons-page>
 </template>
