2184dda4437942930d451989d4442733e47a4cb9
kh06089 <kh06089@ajou.ac.kr>
2019-06-08 21:44:37 +0900

    알람 저장하기

--- frontend/src/pages/Apply_Info.vue
+++ frontend/src/pages/Apply_Info.vue
@@ -32,7 +32,7 @@
                         <ons-icon v-for="n in Math.floor(item.GuideGrade)" icon="fa-star"></ons-icon>
                         <ons-icon v-if="count(item.GuideGrade)" icon="fa-star-half-alt"></ons-icon>
                         {{item.GuideGrade}} / {{item.Total_Review}}명 평가</v-ons-card>
-                    <v-ons-card>여행 진행 건수 : {{item.GuideGrade}}</v-ons-card>
+                    <v-ons-card>여행 진행 건수 : {{item.Total_Tour}}</v-ons-card>
                 </v-ons-card>
             </div>
             <v-ons-list-header>상태: {{item.TourState}} </v-ons-list-header>
